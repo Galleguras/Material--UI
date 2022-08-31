989f631420c578f3991ed4783eeb565285147e2b
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2022-08-31 13:46:52 +0200

    12 Colocando el contenedor y el textfield

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { Container } from "@mui/material";
+import Searcher from "./components/Searcher";
 const App = () => {
-  return <div>Soy un platzinauta</div>;
+  return (
+    <Container
+      sx={{
+        background: "whitesmoke",
+        width: "80vw",
+        height: "500px",
+        borderRadious: "13px",
+        marginTop: "40px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Searcher />
+    </Container>
+  );
 };
 
 export default App;
---
+++ src/components/Searcher/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Stack, TextField, IconButton } from "@mui/material";
+const Searcher = () => {
+  return (
+    <Stack>
+      <TextField />
+      <IconButton></IconButton>
+    </Stack>
+  );
+};
+
+export default Searcher;
