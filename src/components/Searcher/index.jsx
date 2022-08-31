671be2395f0e538aaa3d21bc04552f062802733e
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2022-08-31 14:05:08 +0200

    colocando los estilos del buscador

--- src/components/Searcher/index.jsx
+++ src/components/Searcher/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Stack, TextField, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 const Searcher = () => {
   return (
-    <Stack>
-      <TextField />
-      <IconButton></IconButton>
+    <Stack direction="row" sx={{ marginTop: "30px", width: "80%" }}>
+      <TextField
+        id="outlined-basic"
+        label="Github User"
+        variant="outlined"
+        placeholder="Octocat"
+        sx={{ width: "90%" }}
+        size="small"
+      />
+      <IconButton size="small" sx={{ left: "-45px" }}>
+        <SearchIcon />
+      </IconButton>
     </Stack>
   );
 };
